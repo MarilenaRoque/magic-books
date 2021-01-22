9f608088cb9422e98c3b17cb3a0f4c8332b5671e
MarilenaRoque <marilenas@id.uff.br>
2021-01-22 18:18:08 -0300

    Linters set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 
 function App(props) {
-  const {title} = props;
+  const { title } = props;
   return (
     <>
       <h1> MagicBooks App</h1>
       <h2>{title}</h2>
     </>
-  )
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App title={'Microverse Project'}/>
+    <App title="Microverse Project" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
