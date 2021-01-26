c855fff94251ae86234c8cf47d992dac92bd75c6
MarilenaRoque <marilenas@id.uff.br>
2021-01-26 12:33:41 -0300

    Booklist table structure
    Set initial table structure at Booklist
    Build Book component with rows

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import store from '../store';
+import Book from './Book';
 
 const removeBookX = () => {
   store.dispatch({ type: 'REMOVE_BOOK', payload: { id: 1 } });
@@ -9,12 +10,19 @@ const removeBookX = () => {
 
 function BooksList({ bookList }) {
   console.log(bookList);
-  return (
-    <>
-      <h1 className="books-list">HhHH</h1>
-      <button type="button" onClick={removeBookX}>Hello</button>
-    </>
-  );
+    <button type="button" onClick={removeBookX}>Hello</button>;
+    return (
+      <>
+        <table>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+          <Book id={50} title="Test" category="another test" />
+        </table>
+      </>
+    );
 }
 
 const mapStateToProps = state => ({ bookList: state.books });
