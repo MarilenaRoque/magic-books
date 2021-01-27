42b960663fb73400d6a8b63abb6dac4789947e2e
MarilenaRoque <marilenas@id.uff.br>
2021-01-27 19:09:34 -0300

    Fields State
    Title and Category have a initial state
    HandleChangeTitle and HandleChangeCategory set new States
    Fields have values according to their states

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <form>
-    <label htmlFor="title">
-      Title
-      {' '}
+const BooksForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Kids');
+
+  const handleChangeTitle = input => {
+    setTitle(input);
+  };
+
+  const handleChangeCategory = input => {
+    setCategory(input);
+  };
+
+  return (
+    <form>
+      <label htmlFor="title">
+        Title
+        <br />
+        <input type="text" name="title" value={title} onChange={e => handleChangeTitle(e.target.value)} />
+        <br />
+      </label>
       <br />
-      <input type="text" name="title" />
+      <label htmlFor="categories">
+        Category
+        {' '}
+        <br />
+        <select name="categories" id="categories" value={category} onChange={e => handleChangeCategory(e.target.value)}>
+          {categories.map(item => <option key={item} value={item}>{item}</option>)}
+        </select>
+        <br />
+      </label>
       <br />
-    </label>
-    <br />
-    <label htmlFor="categories">
-      Category
-      {' '}
-      <br />
-      <select name="categories" id="categories">
-        {categories.map(item => <option key={item} value={item}>{item}</option>)}
-      </select>
-      <br />
-    </label>
-    <br />
-    <button type="button" className="add-book">
-      Submit
-    </button>
-  </form>
-);
+      <button type="button" className="add-book">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default BooksForm;
